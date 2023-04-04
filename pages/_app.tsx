b3aaf4b77e2d0bd20633fa2cd586e43cf1121122
Johannes Villasana <Johannes9969@gmail.com>
2023-04-04 11:44:07 -0400

    prueba 1.2 google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,14 +26,21 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" ></meta>
           <link rel="Icon" href="https://website-estelio.s3.amazonaws.com/images/estelio/icono.ico" />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-K869MYHMBF"></script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WLKHPN9');`}
+          </Script>
+          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-K869MYHMBF"></script>
           <script>
             {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
               
             gtag('config', 'G-K869MYHMBF');`}
-          </script>
+          </script> */}
         </Head>
         <ChakraProvider theme={theme}>
           <IpApiContextProvider>
@@ -41,7 +48,10 @@ export default function App({ Component, pageProps }: AppProps) {
             <div className='scrollPath'></div>
             <Component {...pageProps} />
           </IpApiContextProvider>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLKHPN9"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         </ChakraProvider>
+
 
         {/* Google tag (gtag.js) 
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-05K3Q4MH5B" />
