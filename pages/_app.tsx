43d3888603bfcf6796639427ccccfa7ec313de18
Johannes Villasana <Johannes9969@gmail.com>
2023-04-04 12:29:32 -0400

    Production 1.0 google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,11 +26,19 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" ></meta>
           <link rel="Icon" href="https://website-estelio.s3.amazonaws.com/images/estelio/icono.ico" />
+          {/* Production */}
           <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WLKHPN9');`}</script>
+          })(window,document,'script','dataLayer','GTM-NHS2CRZ');`}</script>
+
+          {/* Develop */}
+          {/* <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WLKHPN9');`}</script> */}
 
           {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-K869MYHMBF"></script>
           <script>
@@ -47,8 +55,12 @@ export default function App({ Component, pageProps }: AppProps) {
             <div className='scrollPath'></div>
             <Component {...pageProps} />
           </IpApiContextProvider>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLKHPN9"
+          {/* Production */}
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHS2CRZ"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+          {/* Develop */}
+          {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLKHPN9"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript> */}
         </ChakraProvider>
 
         {/* <Script id="google-analytics" strategy="afterInteractive">
