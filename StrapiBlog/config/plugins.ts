dd23b1187971bfd8c03ea00466514169b16daa75
joseph-eduardo-l <josephlecumberre0@gmail.com>
2025-02-07 09:00:34 -0500

    ADDTOKENNETLIFIY

--- StrapiBlog/config/plugins.ts
+++ StrapiBlog/config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+const pluginsConfig = ({ env }) => ({
+    'netlify-deployments': {
+      enabled: true,
+      config: {
+        accessToken: env('NETLIFY_ACCESS_TOKEN'),
+        sites: [
+          {
+            name: 'strapi360',
+            buildHook: env('NETLIFY_BUILD_HOOK'),
+            branch: 'QA',
+          },
+        ],
+      },
+    },
+  });
+  
+  export default pluginsConfig;
+  
